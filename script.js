ac58b7dbee970d91a489f7dd655eecf8582e6fb9
ishant-raj-2006 <ishant786raj@gmail.com>
2026-01-28 21:25:36 +0530

    Updated Website 1st time

--- script.js
+++ script.js
@@ -15,6 +15,86 @@ const data = [
         Point: 10,
         linkedin: "https://www.linkedin.com/in/riya-singh-703142353?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         github: "https://github.com/riyasingh41996-ctrl"
+    },
+    {
+        rank: 1,
+        Roll: "0255Cybs027",
+        name: "Bhavishya ",
+        Point: 10,
+        linkedin: "https://in.linkedin.com/in/bhavishya-rajput-56a225399",
+        github: "https://github.com/Deon-Wertz"
+    },
+    {
+        rank: 1,
+        Roll: "0255CDS025",
+        name: "Satyam Kumar",
+        Point: 10,
+        linkedin: "https://www.linkedin.com/in/satyam-kumar-5b5167384?utm_source=share_via&utm_content=profile&utm_medium=member_android",
+        github: "https://github.com/satyamkmr18"
+    },
+    {
+        rank: 1,
+        Roll: "0245CYBS019",
+        name: "Prince",
+        Point: 10,
+        linkedin: "https://www.linkedin.com/in/prince-kumar-04b443367?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "https://github.com/Prince-3103"
+    },
+    {
+        rank: 1,
+        Roll: "0245CSE005",
+        name: "Shayef Kabir",
+        Point: 10,
+        linkedin: "http://www.linkedin.com/in/shayef-kabir-b853b0372",
+        github: "https://github.com/shayefkabir2005"
+    },
+    {
+        rank: 1,
+        Roll: "0255CDS015",
+        name: "Muskan Bhari",
+        Point: 10,
+        linkedin: "https://www.linkedin.com/in/muskan-bharti-b9166a3a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "https://github.com/muskan-0228"
+    },
+    {
+        rank: 1,
+        Roll: "0255CSE036",
+        name: "Priyanka Kumari",
+        Point: 10,
+        linkedin: "https://www.linkedin.com/in/priyanka-kumari-5354443a8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "https://github.com/Priyanka-798"
+    },
+    {
+        rank: 2,
+        Roll: "0255CSE039",
+        name: "Antriksh Arya",
+        Point: 5,
+        linkedin: "",
+        github: "https://github.com/antriksharyagrd-a11y"
+    },
+     {
+        rank: 2,
+        Roll: "0255BBA068",
+        name: "Prema Jaiswal",
+        Point: 5,
+        linkedin: "https://www.linkedin.com/in/prema-jaiswal-836658296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: ""
+    },
+    {
+        rank: 0,
+        Roll: "0255CDS035",
+        name: "Prachi Kumari",
+        Point: 0,
+        linkedin: "",
+        github: ""
+    },
+    {
+        rank: 0,
+        Roll: "0255EEE004",
+        name: "Tushar Jaiswal",
+        Point: 0,
+        linkedin: "",
+        github: ""
     }
 ];
 
